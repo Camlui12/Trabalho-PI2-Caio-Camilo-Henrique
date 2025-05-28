064ae503d59d31e08cdce24938630a30b3eb45a2
Camilo Lemos Lima <133181374+Camlui12@users.noreply.github.com>
2025-05-28 18:10:17 -0300

    Iniciando Implementação

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 from views.index import *
+from views.auth import *
 
 if __name__ == '__main__':
     app.run(debug=True)
--- views/index.py
+++ views/index.py
@@ -1,6 +1,8 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, session
 from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'usuario_logado' in session:
+        return render_template('index.html')
+    return redirect(url_for('login'))
