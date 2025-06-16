17c0aa7b85cd2d56f49f3ff988e8ca460a030017
HenriqRocha <hrocha2703@gmail.com>
2025-06-16 13:00:47 -0300

    Cadastro de veículo quase funcionando.
    lógica de cadastrar e já fazer a entrada do veículo feita, não consegui testar pois ainda não sei se temos a população inicial no banco de dados lá já, mas acredito que vai funcionar quando tirar as partes comentadas

--- views/veiculos.py
+++ views/veiculos.py
@@ -1,16 +1,43 @@
 from app import app
 from flask import render_template, request, redirect, url_for, session
-from models import Veiculo
+from models import Veiculo, Estadia
+from datetime import datetime, timezone
 
 
-@app.route('/confirmarEntrada', methods=['POST'])
-
+@app.route('/confirmarEntrada', methods=['GET', 'POST'])
 def confirmarEntrada():#não consegui testar pq n tem nd no banco de dados sobre as placas eu acho    
     carro = Veiculo.query.filter_by(placa=request.form.get('placa')).first()
+    placa = request.form.get('placa')
     if carro:
-        return redirect(url_for('index'))#confimação que entrada foi registrada
+        estadia_ativa = Estadia.query.filter_by(veiculo=placa, saida=None).first()
+        if estadia_ativa:
+            return redirect(url_for('index'))
+        
+        #tarifa_id
+
+        # Cria a nova estadia
+        #nova_estadia = Estadia(entrada=datetime.now(timezone.utc),veiculo=placa,tarifa=tarifa_id)
+        #db.session.add(nova_estadia)
+        #db.session.commit()
+
+        return redirect(url_for('index'))  # Sucesso
     else:
         return render_template('cadastro_veiculo.html')#não sei se aqui é melhor render ou redirect
+    
+
+@app.route('/cadastrarVeiculo', methods=['POST'])
+def cadastrarVeiculo():
+    placa = request.form['placa']
+    modelo = request.form['modelo']
+    cor = request.form['cor']
+    tipo = request.form['tipo']
+
+    newVeiculo = Veiculo(placa = placa, modelo = modelo, cor = cor, tipo = tipo)
+    #db.session.add(newVeiculo)
+    #db.session.comit()
+
+    return redirect(url_for('confirmarEntrada', placa = placa))
+
         
         
        
